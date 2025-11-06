f62b827e30afa00c63158f527d8ff2238a92d2e0
yur-highness <souvickghosh222@gmail.com>
2025-11-07 00:00:20 +0530

    authcontroller updated for production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -43,7 +43,7 @@ export const register = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure:process.env.NODE_ENV === "production",
-            samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 6 * 24 * 60 * 60 * 1000
         });     
 
@@ -115,7 +115,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure:process.env.NODE_ENV === "production",
-            samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 6 * 24 * 60 * 60 * 1000
         });
 
@@ -146,7 +146,7 @@ export const logout = async (req, res) => {
         res.clearCookie("token", {
             httpOnly: true,
             secure:process.env.NODE_ENV === "production",
-            samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         });
         res.status(200).json({
             success: true,
